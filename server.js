ccec4a7813e6c8e045cb185c58d19a3b2be98906
logistics <info@sofane.nl>
2022-12-16 00:37:16 +0100

    Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+const indexRouter = require('./routes/index');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+// Next line is added, will be default in Mongoose 7
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT || 3012);
